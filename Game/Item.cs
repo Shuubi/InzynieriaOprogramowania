719b0a2632b4762b18b161fcdca1161dc594c116
Karolina Więckowska <darkshuubi@gmail.com>
2020-03-20 15:14:26 +0100

    Dodano klasy Player i Item

---
+++ Game/Item.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game
+{
+   public class Item
+    {
+        public int amount { set; get; }
+        string name { set; get; }
+
+        public Item(string n)
+        {
+            name = n;
+            amount = 1;
+        }
+    }
+}
---
+++ Game/PlayerCharacter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game
+{
+   public class PlayerCharacter
+    {
+        public List<Item> inventory;
+
+        public PlayerCharacter()
+        {
+            List<Item> inventory = new List<Item>();
+        }
+    }
+}
