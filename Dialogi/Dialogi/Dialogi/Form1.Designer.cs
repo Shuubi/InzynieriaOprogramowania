443ecd8939d80c013ff0f28b9b82f3ad59513041
MsSalove <62288588+MsSalove@users.noreply.github.com>
2020-03-24 19:18:49 +0100

    Add files via upload
    Dodanie funkcji otwierającej okno do dialogów i czytanie tekstu ze wskazanego pliku
    Do poprawy:
    * Program nie wczytuje pliku po tym jak raz już jakiś wczytał
    * Obsługa plików z kilkoma liniami (oczekiwanie programu na spację zanim przejdzie do czytania dalszej linii
    * Przyspieszanie przewijania (po drugim kliknięciu spacji wczytuje całą linię bez animacji)

---
+++ Dialogi/Dialogi/Dialogi/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace Dialogi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textView = new System.Windows.Forms.Panel();
+            this.Dialog = new System.Windows.Forms.Label();
+            this.textView.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textView
+            // 
+            this.textView.Controls.Add(this.Dialog);
+            this.textView.Location = new System.Drawing.Point(71, 244);
+            this.textView.Name = "textView";
+            this.textView.Size = new System.Drawing.Size(670, 174);
+            this.textView.TabIndex = 0;
+            // 
+            // Dialog
+            // 
+            this.Dialog.AutoSize = true;
+            this.Dialog.Location = new System.Drawing.Point(40, 34);
+            this.Dialog.Name = "Dialog";
+            this.Dialog.Size = new System.Drawing.Size(0, 13);
+            this.Dialog.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textView);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Click += new System.EventHandler(this.clickAction);
+            this.textView.ResumeLayout(false);
+            this.textView.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel textView;
+        private System.Windows.Forms.Label Dialog;
+    }
+}
+
---
+++ Dialogi/Dialogi/Dialogi/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.IO;
+
+namespace Dialogi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        StreamReader sr = new StreamReader(@"C:\Users\Julia\Documents\test.txt");
+
+        private void clickAction(object sender, EventArgs e)
+        {
+            Dialog.Text = "";
+            string ln=String.Empty;
+            string ln2 = String.Empty;
+            
+            if((ln = sr.ReadLine()) != null)
+            {
+                for(int i=0;i<ln.Length;i++)
+                {
+                    ln2 = ln2.Insert(ln2.Length, ln[i].ToString());
+                    Dialog.Text = ln2;
+                    Application.DoEvents();
+                    System.Threading.Thread.Sleep(50);
+                }
+            }
+            
+        }
+    }
+}
